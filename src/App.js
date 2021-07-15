cab1c7307e1feaa66ce7bae676a1c3b564d2d649
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2021-07-15 15:05:05 +0530

    Aliases has setup and added local.properties

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import TestScreen from '@screens/TestScreen';
+
+const App = () => {
+  return (
+    <View>
+      <TestScreen />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
