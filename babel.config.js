ce151506bb3a6238270746b54e109139b2237ebd
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2021-07-18 22:52:56 +0530

    made a relative path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,3 @@
 module.exports = {
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
-    },
-  },
-  plugins: [
-    ['@babel/plugin-proposal-optional-chaining'],
-    [
-      'module-resolver',
-      {
-        alias: {
-          assets: './src/assets',
-          components: './src/components',
-          styles: './src/styles',
-          screens: './src/screens',
-          navigation: './src/navigation',
-          constants: './src/constants',
-        },
-        root: ['./src'],
-      },
-    ],
-  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
