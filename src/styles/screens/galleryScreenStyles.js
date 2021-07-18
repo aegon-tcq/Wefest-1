3b48d6babd0ae0c5200ba29b58fe9d0cb2c02fe0
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2021-07-18 16:41:14 +0530

    completed gallery screen

--- src/styles/screens/galleryScreenStyles.js
+++ src/styles/screens/galleryScreenStyles.js
@@ -4,14 +4,34 @@ import Colors from 'constants/Colors';
 export const galleryScreenStyles = StyleSheet.create({
   listView: {
     flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
   },
   actionsView: {
-    flex: 1,
+    flex: 0.65,
+    paddingVertical: 15,
+    paddingHorizontal: 35,
+    justifyContent: 'center',
+  },
+  actionsView1: {
+    flex: 0.65,
+    paddingVertical: 15,
+    paddingHorizontal: 35,
+    justifyContent: 'center',
   },
   galleryItem: {
-    height: 120,
+    height: 140,
     flex: 1,
-    borderRadius: 20,
+    borderRadius: 10,
     backgroundColor: Colors.primary,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    position: 'relative',
+  },
+  checkboxView: {
+    position: 'absolute',
+    zIndex: 2,
+    right: 10,
+    top: 10,
   },
 });
